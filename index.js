82114ee0fbb5330ca8c02ed92501f175d0adf9dc
FlyingndCoding <94818620+FlyingndCoding@users.noreply.github.com>
2021-12-02 13:26:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const { GCommandsClient } = require("gcommands"),
+    RSSParser = require("rss-parser");
+
+const client = new GCommandsClient({
+    cmdDir: __dirname + "/commands/",
+    language: "english",//
+    commands: {
+        slash: 'slash',
+        context: false,
+        prefix: "." //YOU Can change it
+    },
+    intents: ["GUILDS","GUILD_MESSAGES"]
+})
+
+client.request = new RSSParser();
+client.config = require("./config.json");
+
+client.on("ready", async() => {
+    setInterval(async() => {
+        let ytChannel = client.channels.cache.get(client.config.notificationChannelId);
+        let urlRegexp = /(https?:\/\/[^ ]*)/
+
+        let req = (await client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.youtubeChannelId}`)).items[0];
+
+        let ifAlready = [...(await ytChannel.messages.fetch({ limit: 1 })).values()]
+        if (ifAlready.length > 0) ifAlready = ifAlready[0].content.match(urlRegexp);
+        if (ifAlready != null) ifAlready = ifAlready[1];
+        if (ifAlready == req.link) return;
+
+        ytChannel.send(`${client.config.newVideoMessage.replace(`{url}`,req.link)}`);
+    }, 15000)
+})
+
+client
+    .on("log", console.log)
+    .on("debug", console.log);
+
+client.login(process.env.token)
